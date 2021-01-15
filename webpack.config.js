48eede63a7ce7e86ae1d450477a400a8c144acc7
Ilya Ivanov <static.ila@gmail.com>
2021-01-15 02:09:29 +0200

    Add a prod build version with dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
-module.exports = {
-  entry: './app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin()
-  ]
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+  return {
+    entry: "./app.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProd ? "[chunkhash].js" : "main.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      isProd ? new CleanWebpackPlugin() : undefined,
+    ].filter(x => x),
+  };
 };
